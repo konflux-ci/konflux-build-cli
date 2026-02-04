9e09fd28a6fd5303f0f5984bc59cebd692c3f9c8
Chenxiong Qi <cqi@redhat.com>
2026-02-04 13:27:49 +0800

    Rename COMMAND_NAME in CamelCase
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

--- cmd/image/push_dockerfile.go
+++ cmd/image/push_dockerfile.go
@@ -1,8 +1,6 @@
 package image
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
 	"github.com/konflux-ci/konflux-build-cli/pkg/commands"
@@ -10,14 +8,14 @@ import (
 	l "github.com/konflux-ci/konflux-build-cli/pkg/logger"
 )
 
-const COMMAND_NAME = "push-dockerfile"
+const commandName = "push-dockerfile"
 
 var PushDockerfileCmd = &cobra.Command{
-	Use:   fmt.Sprintf("%s", COMMAND_NAME),
+	Use:   commandName,
 	Short: "Discover Dockerfile from source code and push it to registry as an OCI artifact.",
 	Long:  "Discover Dockerfile from source code and push it to registry as an OCI artifact.",
 	Run: func(cmd *cobra.Command, args []string) {
-		l.Logger.Debugf("Starting %s", COMMAND_NAME)
+		l.Logger.Debugf("Starting %s", commandName)
 		pushDockerfile, err := commands.NewPushDockerfile(cmd)
 		if err != nil {
 			l.Logger.Fatal(err)
@@ -25,7 +23,7 @@ var PushDockerfileCmd = &cobra.Command{
 		if err := pushDockerfile.Run(); err != nil {
 			l.Logger.Fatal(err)
 		}
-		l.Logger.Debugf("Finished %s", COMMAND_NAME)
+		l.Logger.Debugf("Finished %s", commandName)
 	},
 }
 
