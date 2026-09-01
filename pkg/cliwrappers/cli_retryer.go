66916a4fb371c9733ffc1e2ba7eca2e957bba3cf
Emerikh Havryliak <ehavryli@redhat.com>
2026-09-01 09:20:56 +0200

    gofmt cli_retryer.go
    Signed-off-by: Emerikh Havryliak <ehavryli@redhat.com>

--- pkg/cliwrappers/cli_retryer.go
+++ pkg/cliwrappers/cli_retryer.go
@@ -150,7 +150,7 @@ func (r *Retryer) applyJitter(delay time.Duration) time.Duration {
 	if span <= 0 {
 		return delay
 	}
-	return minDelay + rand.N(span + 1)
+	return minDelay + rand.N(span+1)
 }
 
 // StopOnExitCode adds an stop exit code.
